544d541f558eebdbb84b8d68832ccecfe04dc73b
JamesWinestock <james.winestock@gmail.com>
2016-08-22 17:40:30 +1000

    working

--- slack_bot.js
+++ slack_bot.js
@@ -15,7 +15,7 @@ const http = require('http');
 const rule = new schedule.RecurrenceRule();
 rule.dayOfWeek = [0,1,2,3,4,5,6];
 rule.hour = [17];
-rule.minute = [32];
+rule.minute = [35];
 rule.seconds = [00];
 
 // uses the moment module to find the numerical value for the week of the year e.g. the week beggining 8th August 2016 is the 33rd week of the year
@@ -55,7 +55,7 @@ callUsers().then((users) => {
 
   schedule.scheduleJob(rule, function() {
 
-    // if (dayToClean === '1') { //if it's an even week send this webhook
+    { //if it's an even week send this webhook
     bot.sendWebhook({
       text: `${prettyCleaners}`, // prints the first array of names along with the clean message to slack
       channel: '#random', //goes to the random channel
@@ -81,32 +81,5 @@ callUsers().then((users) => {
         }
       ]
     })
-  // } else { //if it's an odd week send this
-  //     bot.sendWebhook({
-  //       text: `${prettyCleaners}`,
-  //       channel: '#random',
-  //       attachments: [
-  //         {
-  //           "text": "Have you cleaned?",
-  //           "color": "#3AA3E3",
-  //           "attachment_type": "default",
-  //           "actions": [
-  //             {
-  //               "name": "yes",
-  //               "text": "Yes",
-  //               "type": "button",
-  //               "value": "yes"
-  //             },
-  //             {
-  //               "name": "no",
-  //               "text": "No",
-  //               "type": "button",
-  //               "value": "no"
-  //             }
-  //           ]
-  //         }
-  //       ]
-  //     })
-  //   }
   })
 })
